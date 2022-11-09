8a15094ff279226f51d276a7fb3c723254a526a1
Ivo Jose <ivojmgs19@gmail.com>
2022-11-09 15:52:01 -0300

    Service layer, component registration

---
+++ src/main/java/com/ivogoncalves/course/domain/enums/OrderStatus.java
@@ -0,0 +1,36 @@
+package com.ivogoncalves.course.domain.enums;
+
+public enum OrderStatus {
+
+	WAIT_PAYMENT(0,"ROLE_WAIT_PAYMENT"), PAID(1,"ROLE_PAID"), SHIPPED(2,"ROLE_SHIPPED"), 
+	DELIVERED(3,"ROLE_DELIVERED"), CANCELED(4,"ROLE_CANCELED");
+	
+	private Integer cod;
+	private String desc;
+	
+	private OrderStatus(Integer cod, String desc) {
+		this.cod = cod;
+		this.desc = desc;
+	}
+	
+	public Integer getCod() {
+		return cod;
+	}
+	
+	public String getDesc() {
+		return desc;
+	}
+	
+	public static OrderStatus toEnum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		for(OrderStatus x : OrderStatus.values()) {
+			if(cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		throw new IllegalArgumentException("Invalid order status!");
+	}
+	
+}
--- src/main/java/com/ivogoncalves/course/services/UserService.java
+++ src/main/java/com/ivogoncalves/course/services/UserService.java
@@ -1,18 +1,27 @@
 package com.ivogoncalves.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ivogoncalves.course.domain.User;
 import com.ivogoncalves.course.repositories.UserRepository;
+import com.ivogoncalves.course.services.exceptions.ObjectNotFoundException;
+
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repository;
+	
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("User not found! Id: " + id));
+	}
 	
 	public List<User> findAll(){
 		return repository.findAll();
